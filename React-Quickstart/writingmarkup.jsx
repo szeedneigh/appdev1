205db0609a73661ea3bfe8a31d05aa855029b6e6
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-03 23:24:43 +0800

    Added tha description/instruction of the Writing markup with JSX section

--- React-Quickstart/writingmarkup.jsx
+++ React-Quickstart/writingmarkup.jsx
@@ -1,5 +1,9 @@
 {/* Writing Markup With JSX*/}
 
+// The markup syntax you've seen above is called JSX. It is optional, but most React projects use JSX for its convenience. All of the tools we recommended for local development support JSX out of the box.
+
+// JSX is stricter that HTML. You have to close tags like <br />. Your component also can't return multiple JSX tags. You have to wrap them into a shared parent, like a <div>...</div> or an empty <>...</> wrapper:
+
 function AboutPage() {
   return (
     <>
@@ -8,3 +12,5 @@ function AboutPage() {
     </>
   );
 }
+
+// If you have a lot of HTML to port to JSX, you can use an online converter.
