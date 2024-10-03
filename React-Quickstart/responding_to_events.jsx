4b7338416e61ee67d1f23421d89833dd9d3e26cd
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-03 19:00:59 +0800

    Done completing the Responding to events section

---
+++ React-Quickstart/responding_to_events.jsx
@@ -0,0 +1,13 @@
+{/* Responding to events */}
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
