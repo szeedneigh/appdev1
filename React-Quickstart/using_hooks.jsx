8dabf66e0035ef92c5126cb2ce869919194e4e97
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-03 22:57:34 +0800

    Done completing the Using Hooks section

---
+++ React-Quickstart/using_hooks.jsx
@@ -0,0 +1,5 @@
+{/* Using hooks*/}
+
+// Functions starting with use are called Hooks. useState is a built-in Hook provided by React. You can find other built-in Hooks in the API reference. You can also write your own Hooks by combining the existing ones.
+
+// Hooks are more restrictive than other functions. You can only call Hooks at the top of your components (or other Hooks). If you want to use useState in a condition or a loop, extract a new component and put it there.
