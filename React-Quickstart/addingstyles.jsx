8ff58ca33d8de7ec1474e2e847fc2ab7f2301bbf
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-04 09:53:56 +0800

    Added the description/instruction of the Adding styles section

--- React-Quickstart/addingstyles.jsx
+++ React-Quickstart/addingstyles.jsx
@@ -1,3 +1,7 @@
 {/* Adding Styles*/}
 
+// In React, you specify a CSS class with className. It works the same way as the HTML class attribute:
+
 <img ClassName="avatar" />
+
+// Then you write the CSSS rules for it in a separate CSS file: see addingstyles.css
