6d5ae8daf972c311884d31de2d81cb699e60448a
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-04 10:22:20 +0800

    Added the description/instruction of the Conditional rendering section

--- React-Quickstart/conditional_rendering.jsx
+++ React-Quickstart/conditional_rendering.jsx
@@ -1,6 +1,7 @@
 {/* Conditional Rendering */}
 
-{/* if statement */}
+// In React, there is no special syntax for writing conditions. Instead, you'll use the same techniques as you use when writing regular JavaScript code. For example, you can use an if statement to conditionally include JSX; 
+
 let content;
 if (isLoggedIn) {
   content = <AdminPanel />;
@@ -13,7 +14,7 @@ return (
   </div>
 );
 
-{/* conditional ? operator */}
+// If you prefer more compact code, you can use the conditional ? operator/ Unlike if, it works inside JSX:
 
 <div>
   {isLoggedIn ? (
@@ -23,8 +24,10 @@ return (
   )}
 </div>
 
-{/* logical && syntax */}
+{/* When you don't need the alse branch, you can also use a shorter logical ?? syntax */}
 
 <div>
   {isLoggedIn && <AdminPanel />}
 </div>
+
+// All these approches also work for conditionally specifying attributes. If you're unfamiliar with some of this JavaScript syntax, you can start by always using if...else
