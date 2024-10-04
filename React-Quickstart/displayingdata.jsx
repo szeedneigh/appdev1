9a8ac39750b47d2b6234c8082cf7ab5a464be4b4
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-04 10:13:50 +0800

    Added the description/instruction of the Displaying data section

--- React-Quickstart/displayingdata.jsx
+++ React-Quickstart/displayingdata.jsx
@@ -1,12 +1,15 @@
 {/* Displaying Data */}
 
+// JSX let you put markup into JavaScript. Curly braces let you "escape back" into JavaScript so that you can embed some variable from you code and display  it to the user. For example, this will display user.name:
+
 return (
   <h1>
     {user.name}
   </h1>
 );
 
-{/* */}
+// You can also "escape int JavaScript" from JSX attributes, but you have to use curly braces instead of quotes. 
+// For example, className='avatar' passes the 'avatar' string as the CSS class, but src={user.imageUrl} reads the JavaScript user.imageUrl variable value, and then passes that value as the src attribute:
 
 return (
   <img 
@@ -15,7 +18,7 @@ return (
   />
 );
 
-{/* */}
+// You can put more complex expressions inside the JSX curly braces too, for example, striung concatenation:
 
 const user = {
   name:'Hedy Lamarr',
@@ -39,3 +42,5 @@ export default function Profile() {
     </>
   );
 }
+
+// In the above example, style={{}} is not special syntax, but a regular {} object inside the style={} JSX curly braces. You can use the style attribute when your styles depend on JavaScript variables.
