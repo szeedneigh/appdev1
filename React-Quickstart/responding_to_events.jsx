80ae504f42fe4d67a4da0a24c45747f871497b0a
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-05 11:18:11 +0800

    Added the description/instruction of the Responding to events section

--- React-Quickstart/responding_to_events.jsx
+++ React-Quickstart/responding_to_events.jsx
@@ -1,5 +1,7 @@
 {/* Responding to events */}
 
+// You can respond to events by declaring event handler functions inside your components:
+
 function MyButton() {
   function handleClick() {
     alert('You clicked me!');
@@ -11,3 +13,5 @@ function MyButton() {
     </button>
   );
 }
+
+// Notice how onClick={handleClick} has no parenthesis at the end! Do not call the event handler function: you only need to pass it down. React will call you event handler when the user clicks the button.
