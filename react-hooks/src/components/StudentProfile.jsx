4bed5788fea2047a3c5bd275041828758c79cfca
szeedneigh <sidneyjohn1704@gmail.com>
2024-11-19 00:07:44 +0800

    Add student profile

---
+++ react-hooks/src/components/StudentProfile.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext } from 'react';
+
+const StudentContext = createContext();
+
+const StudentProvider = ({ children }) => {
+  const student = {
+    name: "John Doe",
+    age: 22,
+    major: 'Bachelor of Science in Information System'
+  };
+
+  return (
+    <StudentContext.Provider value={student}>
+      {children}
+    </StudentContext.Provider>
+  );
+};
+
+const StudentProfile = () => {
+  const student = useContext(StudentContext);
+
+  return (
+    <div>
+      <h2>Student Profile</h2>
+      <p>Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+      <p>Major: {student.major}</p>
+    </div>
+  );
+};
+
+export { StudentProvider, StudentProfile};
--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -6,9 +6,10 @@ import Counter from './components/Counter.jsx'
 import ToggleButton from './components/ToggleButton.jsx'
 import FetchPosts from './components/FetchPosts.jsx'
 import TextInput from './components/TextInput.jsx'
+import { StudentProfile } from './components/StudentProfile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TextInput />
+    <StudentProfile />
   </StrictMode>,
 )
